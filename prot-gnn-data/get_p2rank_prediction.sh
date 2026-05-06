cc9a124bb6554744961fe828aa967fd20233756a
Ethan Huang <xinh0303@163.com>
2026-05-06 22:43:03 +0800

    Add files via upload

---
+++ prot-gnn-data/get_p2rank_prediction.sh
@@ -0,0 +1,14 @@
+# get_p2rank_prediction.sh
+# Author  : Ethan Huang
+# File    : 3.get_p2rank_prediction.sh
+# Time    : 2026/3/25
+
+# Single protein pocket prediction
+# ./prank predict -f /path/to/your/data/glass/pdbs/A5D7K8.pdb -o /path/to/your/data/glass/pdbs-p2rank-test -c alphafold
+
+# Protein list pocket prediction
+# conda deactivate
+# conda activate p2rank
+
+cd /path/to/p2rank/p2rank_2.5.1 # Please change to your own p2rank directory
+./prank predict /path/to/your/data/pdb_list.txt -o /path/to/your/data/pdbs-p2rank-results -threads 20 -c alphafold
